3359ae45d3095200c8fa737d313fa330cdf7337b
Bohuslav Kabrda <bkabrda@redhat.com>
2012-07-13 09:06:09 +0200

    Some minor adjustments in ruby specific patterns

--- rpm2scl/transformers/ruby.py
+++ rpm2scl/transformers/ruby.py
@@ -5,9 +5,10 @@ class RubyTransformer(Transformer):
     def __init__(self, spec, options = None):
         super(RubyTransformer, self).__init__(spec, options)
 
-    @matches(r'(gem (?:install)|(?:unpack)|(?:build))', one_line = False)
-    @matches(r'(testrb )', one_line = False)
-    @matches(r'(testrb2 )', one_line = False)
-    @matches(r'((?<!-)rspec )', one_line = False) # avoid matching stuff like 'rubygem-rspec ' here
+    @matches(r'gem (?:(?:install)|(?:unpack)|(?:build))', one_line = False)
+    @matches(r'testrb ', one_line = False)
+    @matches(r'testrb2 ', one_line = False)
+    @matches(r'(?<!-)rspec ', one_line = False) # avoid matching stuff like 'rubygem-rspec ' here
     def handle_ruby_specific_commands(self, pattern, text):
+        print pattern.search(text).groups()
         return self.sclize_whole_command(pattern, text)
