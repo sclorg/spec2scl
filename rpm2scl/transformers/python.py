d9811ddbeae16dafbf2b98e9831aca216695601b
Bohuslav Kabrda <bkabrda@redhat.com>
2012-07-13 08:55:23 +0200

    Add some basic python specific conversions

---
+++ rpm2scl/transformers/python.py
@@ -0,0 +1,13 @@
+from rpm2scl.decorators import matches
+from rpm2scl.transformers.transformer import Transformer
+
+class PythonTransformer(Transformer):
+    def __init__(self, spec, options = None):
+        super(PythonTransformer, self).__init__(spec, options)
+
+    @matches(r'%{__python\d*} ', one_line = False)
+    @matches(r'nosetests', one_line = False)
+    @matches(r'py.test', one_line = False)
+    @matches(r'sphinx-', one_line = False)
+    def handle_python_specific_commands(self, pattern, text):
+        return self.sclize_whole_command(pattern, text)
