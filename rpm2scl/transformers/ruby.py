0428071f94e5210eecee115c45407cca748baa7a
Bohuslav Kabrda <bkabrda@redhat.com>
2012-07-16 14:15:16 +0200

    Make the ruby regexps more general

--- rpm2scl/transformers/ruby.py
+++ rpm2scl/transformers/ruby.py
@@ -5,9 +5,9 @@ class RubyTransformer(Transformer):
     def __init__(self, spec, options = None):
         super(RubyTransformer, self).__init__(spec, options)
 
-    @matches(r'gem (?:(?:install)|(?:unpack)|(?:build))', one_line = False)
-    @matches(r'testrb ', one_line = False)
-    @matches(r'testrb2 ', one_line = False)
-    @matches(r'(?<!-)rspec ', one_line = False) # avoid matching stuff like 'rubygem-rspec ' here
+    @matches(r'gem\s*(?:(?:install)|(?:unpack)|(?:build))', one_line = False)
+    @matches(r'testrb\s*', one_line = False)
+    @matches(r'testrb2\s*', one_line = False)
+    @matches(r'(?<!-)rspec\s*', one_line = False) # avoid matching stuff like 'rubygem-rspec ' here
     def handle_ruby_specific_commands(self, pattern, text):
         return self.sclize_all_commands(pattern, text)
