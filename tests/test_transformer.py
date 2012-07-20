7bcb82929a0d1c89c8f2e7c943dfab7fbd83ed0a
Bohuslav Kabrda <bkabrda@redhat.com>
2012-07-20 14:25:41 +0200

    Some more tests of the find_whole_commands function

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -12,6 +12,8 @@ class TestGenericConvertor(object):
         (re.compile(r'eat spam'), 'eat spam\neat eat spam', ['eat spam\n', 'eat eat spam']),
         (re.compile(r'eat spam'), 'spam eat\nand spam', []),
         (re.compile(r'eat spam'), 'eat spam \\\n and ham', ['eat spam \\\n and ham']),
+        (re.compile(r'eat spam'), 'SPAM=SPAM eat spam', ['SPAM=SPAM eat spam']),
+        (re.compile(r'eat spam'), 'SPAM=SPAM eat spam \\\n and ham', ['SPAM=SPAM eat spam \\\n and ham']),
     ])
     def test_find_whole_commands(self, pattern, spec, expected):
         assert self.t.find_whole_commands(pattern, spec) == expected
