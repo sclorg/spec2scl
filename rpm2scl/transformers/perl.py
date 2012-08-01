a8d0acbc2bf3e98b2ee1b6c958ebb2e25a38d2c0
Bohuslav Kabrda <bkabrda@redhat.com>
2012-08-01 08:39:02 +0200

    Add Perl specific transformers

---
+++ rpm2scl/transformers/perl.py
@@ -0,0 +1,13 @@
+from rpm2scl.decorators import matches
+from rpm2scl.transformers.transformer import Transformer
+
+class PerlTransformer(Transformer):
+    def __init__(self, spec, options = None):
+        super(PerlTransformer, self).__init__(spec, options)
+
+    @matches(r'%{__perl}\s+', one_line = False)
+    @matches(r'^perl\s+', one_line = False) # carefully here, "perl" will occur often in the specfile
+    @matches(r'./Build', one_line = False)
+    @matches(r'^make\s+', one_line = False) # make is a common word, so don't take it too seriously
+    def handle_perl_specific_commands(self, pattern, text):
+        return self.sclize_all_commands(pattern, text)
