158486bf17e17afcbd6cbf15f0ba1e91120abf2a
Bohuslav Kabrda <bkabrda@redhat.com>
2012-08-01 08:36:18 +0200

    Enable the decorators to compile multiline patterns

--- rpm2scl/decorators.py
+++ rpm2scl/decorators.py
@@ -1,12 +1,17 @@
 import re
 
 def matches(pattern, one_line = True, flags = 0):
+    if one_line:
+        flags = 0
+    else:
+        flags = re.MULTILINE
+
     def inner(func):
         func.one_line = one_line
         if not hasattr(func, 'matches'):
-            func.matches = [re.compile(pattern)]
+            func.matches = [re.compile(pattern, flags)]
         else:
-            func.matches.append(re.compile(pattern))
+            func.matches.append(re.compile(pattern, flags))
         return func
 
     return inner
