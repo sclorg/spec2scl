65309ae01102d6a7821e8aeab2ba133c469a09a4
Bohuslav Kabrda <bkabrda@redhat.com>
2012-09-18 08:43:24 +0200

    Fix the regexp in python transformer

--- spec2scl/transformers/python.py
+++ spec2scl/transformers/python.py
@@ -7,7 +7,7 @@ class PythonTransformer(Transformer):
 
     @matches(r'%{__python\d*}\s+', one_line = False)
     @matches(r'nosetests', one_line = False)
-    @matches(r'py.test', one_line = False)
+    @matches(r'py\.test', one_line = False)
     @matches(r'sphinx-', one_line = False)
     def handle_python_specific_commands(self, pattern, text):
         return self.sclize_all_commands(pattern, text)
