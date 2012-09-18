3fe27c1ea34006e21dbb51178b2f020b3f2a405c
Bohuslav Kabrda <bkabrda@redhat.com>
2012-09-18 10:08:07 +0200

    Some more tests for python transformer

--- tests/test_python_transformer.py
+++ tests/test_python_transformer.py
@@ -10,6 +10,9 @@ class TestPythonTransformer(TransformerTestCase):
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('%{__python} setup.py test\n', '%{?scl:scl enable %{scl} "}\n%{__python} setup.py test\n%{?scl:"}\n'),
+        ('py.test bar\n', '%{?scl:scl enable %{scl} "}\npy.test bar\n%{?scl:"}\n'),
+        ('nosetests foo\n', '%{?scl:scl enable %{scl} "}\nnosetests foo\n%{?scl:"}\n'),
+        ('sphinx-build\n', '%{?scl:scl enable %{scl} "}\nsphinx-build\n%{?scl:"}\n'),
     ])
     def test_python_specific_commands_matching(self, spec, expected):
         patterns = self.t.handle_python_specific_commands.matches
