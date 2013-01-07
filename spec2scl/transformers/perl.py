ec304cd53fb23317b20c83524d701940b201f426
Bohuslav Kabrda <bkabrda@redhat.com>
2013-01-07 10:35:25 +0100

    Fix the infinite loop for certain %{__perl} macro usages

--- spec2scl/transformers/perl.py
+++ spec2scl/transformers/perl.py
@@ -5,7 +5,7 @@ class PerlTransformer(Transformer):
     def __init__(self, spec, options = None):
         super(PerlTransformer, self).__init__(spec, options)
 
-    @matches(r'^[^:]*%{__perl}\s+', one_line = False) # if there is a colon, it is probably a dependency, don't match that
+    @matches(r'^[^:\n]*%{__perl}\s+', one_line = False) # if there is a colon, it is probably a dependency, don't match that
     @matches(r'^perl\s+', one_line = False) # carefully here, "perl" will occur often in the specfile
     @matches(r'./Build', one_line = False)
     def handle_perl_specific_commands(self, pattern, text):
--- tests/test_perl_transformer.py
+++ tests/test_perl_transformer.py
@@ -12,3 +12,13 @@ class TestPerlTransformer(TransformerTestCase):
         spec = 'Requires:       %{?scl_prefix}perl(:MODULE_COMPAT_%(eval "`%{__perl} -V:version`"; echo $version))'
         patterns = self.t.handle_perl_specific_commands.matches
         assert self.get_pattern_for_spec(patterns, spec) == None
+
+    def test_perl_transformer_doesnt_hang_on_perl_macro(self):
+        # the regex was written badly so that it matched the whole spec from the
+        # beginning and find_whole_commands was therefore caught in infinite loop
+        spec = 'foo\n%{__perl} '
+        patterns = self.t.handle_perl_specific_commands.matches
+        for p in patterns:
+            # general transformers also appear here, so just test all to
+            # make sure we test the wanted one, too
+            self.t.find_whole_commands(p, spec)
