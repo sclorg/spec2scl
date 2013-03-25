5fe122c243e115b982a0d4b8faba0791ed862c18
Bohuslav Kabrda <bkabrda@redhat.com>
2013-03-25 15:19:14 +0100

    It should work this way, now implement it :)

--- spec2scl/convertor.py
+++ spec2scl/convertor.py
@@ -1,13 +1,12 @@
 from spec2scl.transformer import Transformer
 from spec2scl.settings import *
+from spec2scl.specfile import Specfile
 
 class Convertor(object):
     def __init__(self, spec, options = None):
         spec = self.list_to_str(spec)
-        # strip changelog, we don't want to transform that
-        changelog_pos = spec.find('%changelog')
-        self.spec = spec[:changelog_pos]
-        self.changelog = spec[changelog_pos:]
+        self.original_spec = spec
+        self.spec = Specfile(spec)
         self.options = options or {}
 
     def list_to_str(self, arg):
@@ -16,4 +15,4 @@ class Convertor(object):
         return arg
 
     def convert(self):
-        return '{0}\n\n{1}'.format(Transformer(self.spec, self.options).transform(), self.changelog)
+        return Transformer(self.original_spec, self.spec, self.options).transform()
