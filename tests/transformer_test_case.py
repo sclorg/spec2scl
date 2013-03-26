6dbd72af13f017d9b1681da49f60aaf69f0a9e41
Bohuslav Kabrda <bkabrda@redhat.com>
2013-03-26 14:29:41 +0100

    Improve our custom test case
    - create a make_prep method that allows quick creation of prep section from anything for good testing of custom transformers (that usually don't transform header section)
    - improve get_pattern_for_spec with section checking

--- tests/transformer_test_case.py
+++ tests/transformer_test_case.py
@@ -1,6 +1,15 @@
+from spec2scl import settings
+from spec2scl import specfile
+
 class TransformerTestCase(object):
-    def get_pattern_for_spec(self, patterns, spec):
-        for pattern in patterns:
-            if pattern.search(spec):
-                return pattern
+    def make_prep(self, spec):
+        # just create one of settings.RUNTIME_SECTIONS, so that we can test all the matching
+        return '%prep\n' + spec
+
+    def get_pattern_for_spec(self, handler, spec_text):
+        spec = specfile.Specfile(spec_text)
+        for s_name, s_text in spec.sections:
+            for i, pattern in enumerate(handler.matches):
+                if pattern.search(s_text) and s_name in handler.sections[i]:
+                    return pattern
 
