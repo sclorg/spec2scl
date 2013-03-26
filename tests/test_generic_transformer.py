f5f5d9fab5b730061cc12c8f97b72958890eea11
Bohuslav Kabrda <bkabrda@redhat.com>
2013-03-26 12:50:09 +0100

    Fix test_generic_transformer after the refactoring

--- tests/test_generic_transformer.py
+++ tests/test_generic_transformer.py
@@ -6,7 +6,7 @@ from tests.transformer_test_case import TransformerTestCase
 
 class TestGenericTransformer(TransformerTestCase):
     def setup_method(self, method):
-        self.t = GenericTransformer('', {})
+        self.t = GenericTransformer({})
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('', '%{?scl:%scl_package TODO}\n%{!?scl:%global pkg_name %{name}}'),
@@ -16,7 +16,7 @@ class TestGenericTransformer(TransformerTestCase):
     ])
     def test_insert_scl_init(self, spec, expected):
         self.t.original_spec = spec # need to assign it here, because Name is taken from original spec
-        assert self.t.insert_scl_init(self.t.insert_scl_init.matches[0], spec).find(expected) != -1
+        assert self.t.insert_scl_init(spec, self.t.insert_scl_init.matches[0], spec).find(expected) != -1
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('Conflicts:spam', 'Conflicts:%{?scl_prefix}spam'),
@@ -28,7 +28,7 @@ class TestGenericTransformer(TransformerTestCase):
     ])
     def test_handle_dependency_tag(self, spec, expected):
         patterns = self.t.handle_dependency_tag.matches
-        assert self.t.handle_dependency_tag(self.get_pattern_for_spec(patterns, spec), spec) == expected
+        assert self.t.handle_dependency_tag(spec, self.get_pattern_for_spec(patterns, spec), spec) == expected
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('Requires: perl(:MODULE_COMPAT_%(eval "`perl -V:version`"; echo $version))',
@@ -39,7 +39,7 @@ class TestGenericTransformer(TransformerTestCase):
     ])
     def test_handle_dependency_tag_with_spaces_in_brackets(self, spec, expected):
         patterns = self.t.handle_dependency_tag.matches
-        assert self.t.handle_dependency_tag(self.get_pattern_for_spec(patterns, spec), spec) == expected
+        assert self.t.handle_dependency_tag(spec, self.get_pattern_for_spec(patterns, spec), spec) == expected
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('Requires: /foo/spam', 'Requires: %{?_scl_root}/foo/spam'),
@@ -47,7 +47,7 @@ class TestGenericTransformer(TransformerTestCase):
     ])
     def test_handle_dependency_tag_with_path(self, spec, expected):
         patterns = self.t.handle_dependency_tag.matches
-        assert self.t.handle_dependency_tag(self.get_pattern_for_spec(patterns, spec), spec) == expected
+        assert self.t.handle_dependency_tag(spec, self.get_pattern_for_spec(patterns, spec), spec) == expected
 
     @pytest.mark.parametrize(('spec', 'scl_requires', 'expected'), [
         ('Requires: spam = %{epoch}:%{version}-%{release}', 'a', 'Requires: %{?scl_prefix}spam = %{epoch}:%{version}-%{release}'),
@@ -62,7 +62,7 @@ class TestGenericTransformer(TransformerTestCase):
         patterns = self.t.handle_dependency_tag_modified_by_list.matches
         if scl_requires:
             self.t.options = {'scl_requires': scl_requires}
-        assert self.t.handle_dependency_tag_modified_by_list(self.get_pattern_for_spec(patterns, spec), spec) == expected
+        assert self.t.handle_dependency_tag_modified_by_list(spec, self.get_pattern_for_spec(patterns, spec), spec) == expected
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('%package \t -n spam', '%package \t -n %{?scl_prefix}spam'),
@@ -71,7 +71,7 @@ class TestGenericTransformer(TransformerTestCase):
     ])
     def test_handle_subpackages_should_sclize(self, spec, expected):
         patterns = self.t.handle_subpackages.matches
-        assert self.t.handle_subpackages(self.get_pattern_for_spec(patterns, spec), spec) == expected
+        assert self.t.handle_subpackages(spec, self.get_pattern_for_spec(patterns, spec), spec) == expected
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('%package spam', '%package spam'),
@@ -87,14 +87,14 @@ class TestGenericTransformer(TransformerTestCase):
         ('%setup -q -c -T', '%setup -n %{pkg_name}-%{version} -q -c -T'),
     ])
     def test_handle_setup_macro_should_sclize(self, spec, expected):
-        assert self.t.handle_setup_macro(self.t.handle_setup_macro.matches[0], spec) == expected
+        assert self.t.handle_setup_macro(spec, self.t.handle_setup_macro.matches[0], spec) == expected
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('%setup -n spam', '%setup -n spam'),
         ('%setup -q -n spam -c', '%setup -q -n spam -c'),
     ])
     def test_handle_setup_macro_should_not_sclize(self, spec, expected):
-        assert self.t.handle_setup_macro(self.t.handle_setup_macro.matches[0], spec) == expected
+        assert self.t.handle_setup_macro(spec, self.t.handle_setup_macro.matches[0], spec) == expected
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('Name: spam', 'Name: %{?scl_prefix}spam'),
@@ -102,7 +102,7 @@ class TestGenericTransformer(TransformerTestCase):
         ('Name:  %{?spam}spam', 'Name:  %{?scl_prefix}%{?spam}spam'),
     ])
     def test_handle_name_tag(self, spec, expected):
-        assert self.t.handle_name_tag(self.t.handle_name_tag.matches[0], spec) == expected
+        assert self.t.handle_name_tag(spec, self.t.handle_name_tag.matches[0], spec) == expected
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('%{name}', '%{pkg_name}'),
@@ -110,7 +110,7 @@ class TestGenericTransformer(TransformerTestCase):
         ('%{name_spam}', '%{name_spam}'),
     ])
     def test_handle_name_macro(self, spec, expected):
-        assert self.t.handle_name_macro(self.t.handle_name_macro.matches[0], spec) == expected
+        assert self.t.handle_name_macro(spec, self.t.handle_name_macro.matches[0], spec) == expected
 
     @pytest.mark.parametrize(('spec', 'meta_runtime_dep', 'expected'), [
         ('Requires:', False, 'Requires:'),
@@ -118,7 +118,7 @@ class TestGenericTransformer(TransformerTestCase):
     ])
     def test_handle_meta_runtime_dep(self, spec, meta_runtime_dep, expected):
         self.t.options['meta_runtime_dep'] = meta_runtime_dep
-        assert self.t.handle_meta_runtime_dep(None, spec) == expected
+        assert self.t.handle_meta_runtime_dep(spec, None, spec) == expected
 
     @pytest.mark.parametrize(('spec', 'expected'), [
         ('configure\n', '%{?scl:scl enable %{scl} "}\nconfigure\n%{?scl:"}\n'),
@@ -129,4 +129,4 @@ class TestGenericTransformer(TransformerTestCase):
     ])
     def test_handle_name_macro(self, spec, expected):
         patterns = self.t.handle_configure_make.matches
-        assert self.t.handle_configure_make(self.get_pattern_for_spec(patterns, spec), spec) == expected
+        assert self.t.handle_configure_make(spec, self.get_pattern_for_spec(patterns, spec), spec) == expected
