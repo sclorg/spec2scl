4e03a75271910eb0ca5e0460a7f5fe657b7131e4
Robert Kuska <rkuska@redhat.com>
2014-04-28 10:26:15 +0200

    Exclude file based requires from sclizing, fixes #5

--- spec2scl/transformers/generic.py
+++ spec2scl/transformers/generic.py
@@ -4,6 +4,7 @@ from spec2scl.decorators import matches
 from spec2scl import settings
 from spec2scl import transformer
 
+
 @transformer.Transformer.register_transformer
 class GenericTransformer(transformer.Transformer):
     def __init__(self, options={}):
@@ -14,7 +15,7 @@ class GenericTransformer(transformer.Transformer):
         scl_init = '%{{?scl:%scl_package {0}}}\n%{{!?scl:%global pkg_name %{{name}}}}'.format(self.get_original_name(original_spec))
         return '{0}\n\n{1}'.format(scl_init, text)
 
-    @matches(r'(?<!d)(Conflicts:\s*)([^\s]+)', sections=settings.METAINFO_SECTIONS) # avoid BuildConflicts
+    @matches(r'(?<!d)(Conflicts:\s*)([^\s]+)', sections=settings.METAINFO_SECTIONS)  # avoid BuildConflicts
     @matches(r'(BuildConflicts:\s*)([^\s]+)', sections=settings.METAINFO_SECTIONS)
     @matches(r'(Provides:\s*)([^\s]+)', sections=settings.METAINFO_SECTIONS)
     @matches(r'(Obsoletes:\s*)([^\s]+)', sections=settings.METAINFO_SECTIONS)
@@ -22,6 +23,7 @@ class GenericTransformer(transformer.Transformer):
         tag = text[0:text.find(':') + 1]
         deps = text[text.find(':') + 1:]
         # handle more Requires on one line
+
         def handle_one_dep(matchobj):
             groupdict = matchobj.groupdict('')
 
@@ -40,8 +42,8 @@ class GenericTransformer(transformer.Transformer):
         dep_re = re.compile(r'(?P<prespace>\s*)(?P<dep>([^\s]+(.+\))?))(?P<ver>\s*[<>=!]+\s*[^\s]+)?(?P<postspace>\s*)')
         return tag + dep_re.sub(handle_one_dep, deps)
 
-    @matches(r'(?<!d)(Requires:\s*)([^[\s]+)', sections=settings.METAINFO_SECTIONS) # avoid BuildRequires
-    @matches(r'(BuildRequires:\s*)([^\s]+)', sections=settings.METAINFO_SECTIONS)
+    @matches(r'(?<!d)(Requires:\s*)(?!\w*/\w*)([^[\s]+)', sections=settings.METAINFO_SECTIONS)  # avoid BuildRequires
+    @matches(r'(BuildRequires:\s*)(?!\w*/\w*)([^\s]+)', sections=settings.METAINFO_SECTIONS)
     def handle_dependency_tag_modified_by_list(self, original_spec, pattern, text):
         return self.handle_dependency_tag(pattern, original_spec, text, True)
 
