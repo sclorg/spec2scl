83c6fbf0efeba0830deeeba8824174c779a95208
Mat Booth <mat.booth@redhat.com>
2017-11-10 16:29:08 +0000

    Make spec2scl aware of scriptlet runtime sections
    Ensures correct sclisation of spec files that contain scriptlet
    sections such as %pre and %post
    
    Additional test case parameter added

--- spec2scl/settings.py
+++ spec2scl/settings.py
@@ -9,10 +9,16 @@ SPECFILE_SECTIONS = ['%header',  # special "section" for the start of specfile
                      '%install',
                      '%clean',
                      '%check',
+                     '%pre',
+                     '%post',
+                     '%preun',
+                     '%postun',
+                     '%pretrans',
+                     '%posttrans',
                      '%files',
                      '%changelog']
 
-RUNTIME_SECTIONS = ['%prep', '%build', '%install', '%clean', '%check']
+RUNTIME_SECTIONS = ['%prep', '%build', '%install', '%clean', '%check', '%pre', '%post', '%preun', '%postun', '%pretrans', '%posttrans']
 METAINFO_SECTIONS = ['%header', '%package']
 
 SCL_ENABLE = '%{?scl:scl enable %{scl} - << \EOF}\nset -ex\n'
