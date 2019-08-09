5ba9b50e1d38d156a21178c3969bc6987b368f2b
Jan Staněk <jstanek@redhat.com>
2019-08-09 13:19:20 +0200

    Fix unintentional escape sequences
    Improve formatting of relevant files

--- spec2scl/settings.py
+++ spec2scl/settings.py
@@ -1,25 +1,39 @@
 """Transformation related settings and constants.
 """
 
-SPECFILE_SECTIONS = ['%header',  # special "section" for the start of specfile
-                     '%description',
-                     '%package',
-                     '%prep',
-                     '%build',
-                     '%install',
-                     '%clean',
-                     '%check',
-                     '%pre',
-                     '%post',
-                     '%preun',
-                     '%postun',
-                     '%pretrans',
-                     '%posttrans',
-                     '%files',
-                     '%changelog']
+SPECFILE_SECTIONS = [
+    "%header",  # special "section" for the start of specfile
+    "%description",
+    "%package",
+    "%prep",
+    "%build",
+    "%install",
+    "%clean",
+    "%check",
+    "%pre",
+    "%post",
+    "%preun",
+    "%postun",
+    "%pretrans",
+    "%posttrans",
+    "%files",
+    "%changelog",
+]
 
-RUNTIME_SECTIONS = ['%prep', '%build', '%install', '%clean', '%check', '%pre', '%post', '%preun', '%postun', '%pretrans', '%posttrans']
-METAINFO_SECTIONS = ['%header', '%package']
+RUNTIME_SECTIONS = [
+    "%prep",
+    "%build",
+    "%install",
+    "%clean",
+    "%check",
+    "%pre",
+    "%post",
+    "%preun",
+    "%postun",
+    "%pretrans",
+    "%posttrans",
+]
+METAINFO_SECTIONS = ["%header", "%package"]
 
-SCL_ENABLE = '%{?scl:scl enable %{scl} - << \EOF}\nset -ex\n'
-SCL_DISABLE = '%{?scl:EOF}\n'
+SCL_ENABLE = "%{?scl:scl enable %{scl} - << \\EOF}\nset -ex\n"
+SCL_DISABLE = "%{?scl:EOF}\n"
--- tests/transformer_test_case.py
+++ tests/transformer_test_case.py
@@ -1,14 +1,14 @@
 from spec2scl import specfile
 
-
-scl_enable = '%{?scl:scl enable %{scl} - << \EOF}\nset -ex\n'
-scl_disable = '%{?scl:EOF}\n'
+scl_enable = "%{?scl:scl enable %{scl} - << \\EOF}\nset -ex\n"
+scl_disable = "%{?scl:EOF}\n"
 
 
 class TransformerTestCase(object):
     def make_prep(self, spec):
-        # just create one of settings.RUNTIME_SECTIONS, so that we can test all the matching
-        return '%prep\n' + spec
+        # just create one of settings.RUNTIME_SECTIONS,
+        # so that we can test all the matching
+        return "%prep\n" + spec
 
     def get_pattern_for_spec(self, handler, spec_text):
         spec = specfile.Specfile(spec_text)
